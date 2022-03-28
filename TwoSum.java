40c4612f4a8aa5815df4f4e020916191d638e4aa
trane <xq1281459782@gmail.com>
2022-03-29 02:41:48 +0800

    剑指 Offer II 006. 排序数组中两个数字之和

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,7 @@
 /**
  * @className: TwoSum
  * @description: 167. 两数之和 II - 输入有序数组
+ *               剑指 Offer II 006. 排序数组中两个数字之和
  * @author: Carl Tong
  * @date: 2022/3/17 21:31
  */
@@ -8,11 +9,10 @@ public class TwoSum {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1;
         while (left < right) {
-            int sum = numbers[left] + numbers[right];
-            if (sum == target) break;
-            else if (sum < target) left++;
-            else right--;
+            while (numbers[left] + numbers[right] < target) left++;
+            while (numbers[left] + numbers[right] > target) right--;
+            if (numbers[left] + numbers[right] == target) break;
         }
-        return new int[]{left + 1, right + 1};
+        return new int[]{left, right};
     }
 }
