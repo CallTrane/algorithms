579049a366cc72e7bae17b504b50e715ac91d698
trane <xq1281459782@gmail.com>
2022-04-24 11:45:45 +0800

    239. 滑动窗口最大值 剑指 Offer 59 - I. 滑动窗口的最大值

--- MaxSlidingWindow.java
+++ MaxSlidingWindow.java
@@ -1,41 +1,34 @@
-import java.util.Deque;
 import java.util.LinkedList;
 
 /**
  * @className: MaxSlidingWindow
- * @description: 剑指 Offer 59 - I. 滑动窗口的最大值
+ * @description: 239. 滑动窗口最大值
+ *              剑指 Offer 59 - I. 滑动窗口的最大值
  * @author: carl
  * @date: 2021/8/21 10:21
  */
 public class MaxSlidingWindow {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || k == 0) {
-            return new int[0];
-        }
-        Deque<Integer> deque = new LinkedList();
-        int[] result = new int[nums.length - k + 1];
-        // 当 i < k ，说明还没有形成窗口
-        for (int i = 0; i < k; i++) {
-            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
-                deque.removeLast();
+        // 单调队列解法，保证队首到队尾是单调递减的
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        // 先填充窗口到即将计算最值的时候
+        for (int i = 0; i < k - 1; i++) {
+            // 从队尾开始，比当前元素小的都不要，保证入队了也是单调的
+            while (!queue.isEmpty() && queue.peekLast() < nums[i]) {
+                queue.removeLast();
             }
-            deque.addLast(nums[i]);
+            queue.addLast(nums[i]);
         }
-        // 形成窗口
-        result[0] = deque.peekFirst();
-        // 开始滑动窗口
-        for (int i = k; i < nums.length; i++) {
-            // 若 i > 0 且 队首元素 deque[0] == 被删除元素 nums[i - 1] ：则队首元素出队；
-            if (deque.peekFirst() == nums[i - k]) {
-                deque.removeFirst();
-            }
-            // 删除 deque 内所有 < nums[j] 的元素，以保持 deque 递减
-            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
-                deque.removeLast();
-            }
-            deque.addLast(nums[i]);
-            result[i - k + 1] = deque.peekFirst();
+        // 计算最值
+        for (int i = k - 1; i < nums.length; i++) {
+            while (!queue.isEmpty() && queue.peekLast() < nums[i]) queue.removeLast();
+            queue.addLast(nums[i]);
+            // 当前窗口答案
+            res[i - k + 1] = queue.peekFirst();
+            // 如果要移除的数刚好为窗口最值，就出队
+            if (nums[i - k + 1] == queue.peekFirst()) queue.pop();
         }
-        return result;
+        return res;
     }
 }
