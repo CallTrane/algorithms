2727ba1dcde1f2d6b19dd8143eb81332576de950
trane <xq1281459782@gmail.com>
2022-04-25 20:52:35 +0800

    改一下

--- Kuaishou.java
+++ Kuaishou.java
@@ -5,13 +5,9 @@
  * @date: 2021/9/4 10:35
  */
 public class Kuaishou {
-    public static void main(String[] args) {
-        int c=1+1; Integer d=1+1;
-        Long e= 1+1L;
-        System.out.println(d.equals(c)); //true
-        System.out.println(c==d); //true
-        System.out.println(d.equals(e)); //false
-    }
 
+    static class TestOne {
+
+    }
 
 }
