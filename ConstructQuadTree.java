6684ddd49495c949ecb83cebaee4f19997f849b0
trane <xq1281459782@gmail.com>
2022-05-01 12:00:44 +0800

    427. 建立四叉树

---
+++ ConstructQuadTree.java
@@ -0,0 +1,49 @@
+/**
+ * @className: ConstructQuadTree
+ * @description: 427. 建立四叉树
+ * @author: Carl Tong
+ * @date: 2022/4/29 11:28
+ */
+public class ConstructQuadTree {
+
+    /*public Node construct(int[][] grid) {
+
+    }
+
+    class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+
+
+        public Node() {
+            this.val = false;
+            this.isLeaf = false;
+            this.topLeft = null;
+            this.topRight = null;
+            this.bottomLeft = null;
+            this.bottomRight = null;
+        }
+
+        public Node(boolean val, boolean isLeaf) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = null;
+            this.topRight = null;
+            this.bottomLeft = null;
+            this.bottomRight = null;
+        }
+
+        public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+    }*/
+}
