9f7a4f35ddd995624afd2fc8b83b1c9f9206513f
trane <xq1281459782@gmail.com>
2022-05-02 11:30:41 +0800

    105. 从前序与中序遍历序列构造二叉树

--- BuildTree_1.java
+++ BuildTree_1.java
@@ -24,32 +24,33 @@ public class BuildTree_1 {
         }
     }
 
-    HashMap<Integer, Integer> inOrder;
+    // 题目说二叉树节点的值不存在重复，所以可以使用一个 HashMap 存储元素到索引的映射
+    Map<Integer, Integer> inOrderIndex = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length != preorder.length) {
-            return null;
-        }
-        inOrder = new HashMap();
+        // 存储中序遍历的索引
         for (int i = 0; i < inorder.length; i++) {
-            inOrder.put(inorder[i], i);
+            inOrderIndex.put(inorder[i], i);
         }
-        return buildTree(preorder, 0, preorder.length-1,
-                inOrder, 0, inorder.length-1);
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
-    private TreeNode buildTree(int[] preorder, int preLeft, int preRight,
-                               Map<Integer, Integer> inorder, int inLeft, int inRight) {
-        if (preLeft > preRight || inLeft > inRight) {
-            return null;
-        }
-        // index - 1 - inLeft = x - (preLeft + 1)
-        int rootVal = preorder[preLeft];
+    private TreeNode buildTree(int[] preorder, int preStart, int preEnd,
+                              int[] inorder, int inStart, int inEnd) {
+        // 递归终止条件
+        if (preStart > preEnd || inStart > inEnd) return null;
+        // 确定根的值
+        int rootVal = preorder[preStart];
+        // 在中序遍历中的索引位置
+        int index = inOrderIndex.get(rootVal);
+        // 左子树的节点数
+        int leftSize = index - inStart;
+        // 构建
         TreeNode root = new TreeNode(rootVal);
-        int index = inorder.get(rootVal);
-
-        root.left = buildTree(preorder, preLeft+1, preLeft+index-inLeft, inorder, inLeft, index-1);
-        root.right = buildTree(preorder, preLeft+index-inLeft+1, preRight, inorder, index+1, inRight);
+        // 根据上面计算的左子树节点数，规划范围
+        root.left = buildTree(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1);
+        root.right = buildTree(preorder, preStart + leftSize + 1, preEnd, inorder, index + 1, inEnd);
         return root;
     }
+
 }
