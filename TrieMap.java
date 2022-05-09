710cd465fa4e7c97114c5c0130f8bc18badefce9
trane <xq1281459782@gmail.com>
2022-05-10 00:34:17 +0800

    Trie树、字典树、前缀树

--- TrieMap.java
+++ TrieMap.java
@@ -263,7 +263,7 @@ public class TrieMap<V> {
     /**
      * 在 Map 的所有键中搜索 query 的最短前缀
      */
-    public String shorestPrefixOf(String query) {
+    public String shortestPrefixOf(String query) {
         TrieNode<V> p = root;
         for (int i = 0; i < query.length(); i++) {
             // 结点为空，没有前缀，无法继续向下搜索
@@ -299,4 +299,66 @@ public class TrieMap<V> {
         return query.substring(0, maxLength);
     }
 
+}
+
+class TrieSet {
+    // 底层用一个 TrieMap，键就是 TrieSet，值仅仅起到占位的作用
+    // 值的类型可以随便设置，参考 Java 标准库设置成 Object
+    private final TrieMap<Object> map = new TrieMap<>();
+
+    /***** 增 *****/
+
+    // 在集合中添加元素 key
+    public void add(String key) {
+        map.put(key, new Object());
+    }
+
+    /***** 删 *****/
+
+    // 从集合中删除元素 key
+    public void remove(String key) {
+        map.remove(key);
+    }
+
+    /***** 查 *****/
+
+    // 判断元素 key 是否存在集合中
+    public boolean contains(String key) {
+        return map.containsKey(key);
+    }
+
+    // 在集合中寻找 query 的最短前缀
+    public String shortestPrefixOf(String query) {
+        return map.shortestPrefixOf(query);
+    }
+
+    // 在集合中寻找 query 的最长前缀
+    public String longestPrefixOf(String query) {
+        return map.longestPrefixOf(query);
+    }
+
+    // 在集合中搜索前缀为 prefix 的所有元素
+    public List<String> keysWithPrefix(String prefix) {
+        return map.keysWithPrefix(prefix);
+    }
+
+    // 判断集合中是否存在前缀为 prefix 的元素
+    public boolean hasKeyWithPrefix(String prefix) {
+        return map.hasKeyWithPrefix(prefix);
+    }
+
+    // 通配符 . 匹配任意字符，返回集合中匹配 pattern 的所有元素
+    public List<String> keysWithPattern(String pattern) {
+        return map.keysWithPattern(pattern);
+    }
+
+    // 通配符 . 匹配任意字符，判断集合中是否存在匹配 pattern 的元素
+    public boolean hasKeyWithPattern(String pattern) {
+        return map.hasKeyWithPattern(pattern);
+    }
+
+    // 返回集合中元素的个数
+    public int size() {
+        return map.size();
+    }
 }
