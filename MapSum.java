164ce7b18f31911b90acb990f62ee16eff4d2957
trane <xq1281459782@gmail.com>
2022-05-13 20:56:02 +0800

    677. 键值映射

--- MapSum.java
+++ MapSum.java
@@ -21,7 +21,7 @@ public class MapSum {
 
     public int sum(String prefix) {
         List<String> keys = TRIE_MAP.keysWithPrefix(prefix);
-        return keys.stream().mapToInt(key -> TRIE_MAP.get(key)).sum();
+        return keys.stream().mapToInt(TRIE_MAP::get).sum();
     }
 
     private class TrieMap<V> {
