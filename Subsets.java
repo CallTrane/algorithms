3332329035159d214697b329d641cae85704c8c3
trane <xq1281459782@gmail.com>
2022-05-14 12:14:08 +0800

    78. 子集

--- Subsets.java
+++ Subsets.java
@@ -11,15 +11,23 @@ import java.util.List;
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        track(nums, 0, new LinkedList<>(), res);
+        // 记录回溯算法的递归路径
+        LinkedList<Integer> track = new LinkedList<>();
+        track(nums, 0, track, res);
         return res;
     }
 
+    // 回溯算法核心函数，遍历子集问题的回溯树
     private void track(int[] nums, int start, LinkedList<Integer> track, List<List<Integer>> res) {
+        // 前序位置，每个节点的值都是一个子集
         res.add(new LinkedList<>(track));
+        // 回溯算法标准框架
         for (int i = start; i < nums.length; i++) {
+            // 做选择
             track.addLast(nums[i]);
+            // i 通过 start 参数控制树枝的遍历，避免产生重复的子集
             track(nums, i + 1, track, res);
+            // 撤销选择
             track.removeLast();
         }
     }
