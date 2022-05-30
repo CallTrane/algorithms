dab96972d2be806576edf43734f8e999a4c75722
trane <xq1281459782@gmail.com>
2022-05-30 14:29:36 +0800

    699. 掉落的方块 （区间树相关，待做）

---
+++ 树/区间树/FallingSquares.java
@@ -0,0 +1,15 @@
+package 树.区间树;
+
+import java.util.List;
+
+/**
+ * @className: FallingSquares
+ * @description: 699. 掉落的方块
+ * @author: Carl Tong
+ * @date: 2022/5/26 19:23
+ */
+public class FallingSquares {
+    /*public List<Integer> fallingSquares(int[][] positions) {
+
+    }*/
+}
