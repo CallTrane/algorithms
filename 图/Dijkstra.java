8a87eb8066b5841feb77b1e24df2701994ad39ef
trane <xq1281459782@gmail.com>
2022-05-30 19:27:32 +0800

    Dijkstra's algorithm : 解决的是带权重(非负值)的有向图上单源最短路径问题

---
+++ 图/Dijkstra.java
@@ -0,0 +1,69 @@
+package 图;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @className: Dijkstra
+ * @description: Dijkstra's algorithm : 解决的是带权重(非负值)的有向图上单源最短路径问题
+ * @author: Carl Tong
+ * @date: 2022/5/26 16:41
+ */
+public class Dijkstra {
+
+    class Node {
+        // 图节点的 id
+        int id;
+        // 从 start 节点到当前节点的距离
+        int distFromStart;
+
+        public Node(int id, int distFromStart) {
+            this.id = id;
+            this.distFromStart = distFromStart;
+        }
+    }
+
+    private List<Integer>[] graph;
+
+    // 返回节点 from 到节点 to 之间的边的权重
+    private int weight(int from, int to) {
+        // 根据问题计算权重
+        return 0; // 这里的0是随便写的
+    }
+
+    // 输入节点 s, 返回 s 的相邻节点
+    private List<Integer> adj(int s) {
+        return graph[s];
+    }
+
+    // 输入一幅图和一个起点 start，计算 start 到其他节点的最短距离
+    public int[] dijkstra(int start, List<Integer>[] graph) {
+        // 定义：distTo[i] 的值就是节点 start 到达节点 i 的最短路径权重 (记录最短路径的权重, 可以理解为 dp table)
+        int[] distTo = new int[graph.length];
+        // 求最小值，所以 dp table 初始化为正无穷
+        Arrays.fill(distTo, Integer.MAX_VALUE);
+        // base case
+        distTo[start] = 0;
+        // 优先队列，distFromStart 较小的排在前面
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.distFromStart));
+        pq.offer(new Node(start, 0));
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            int curNodeId = curNode.id, curNodeDistFromStart = curNode.distFromStart;
+            // 已经有一条更短的路径到达 curNode 节点了
+            if (curNodeDistFromStart > distTo[curNodeId]) continue;
+            // 否则, 通过 curNode 节点, 计算到相邻节点的距离会不会更短
+            for (int nextNodeId : adj(curNodeId)) {
+                int nextNodeDistFromStart = curNodeDistFromStart + weight(nextNodeId, curNodeId);
+                // 如果距离可以更短, 则更新 dp table 且入队
+                if (nextNodeDistFromStart < distTo[nextNodeId]) {
+                    distTo[nextNodeId] = nextNodeDistFromStart;
+                    pq.offer(new Node(nextNodeId, nextNodeDistFromStart));
+                }
+            }
+        }
+        return distTo;
+    }
+}
